efa0551241f4f626c6f2c4bfae3a21026153dc90
Volha Pivavarchyk <volha@aivie.ch>
2023-08-03 13:14:43 +0300

    Remove unnecessary code

--- Form/Extension/FormFieldSelectTypeExtension.php
+++ Form/Extension/FormFieldSelectTypeExtension.php
@@ -6,14 +6,10 @@ namespace MauticPlugin\CustomObjectsBundle\Form\Extension;
 
 use Mautic\FormBundle\Form\Type\FormFieldGroupType;
 use Mautic\FormBundle\Form\Type\FormFieldSelectType;
-use MauticPlugin\CustomObjectsBundle\Model\CustomItemModel;
 use MauticPlugin\CustomObjectsBundle\Model\CustomObjectModel;
-use MauticPlugin\CustomObjectsBundle\Repository\CustomItemXrefContactRepository;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 
 class FormFieldSelectTypeExtension extends AbstractTypeExtension
 {
@@ -38,35 +34,7 @@ class FormFieldSelectTypeExtension extends AbstractTypeExtension
     {
         parent::buildForm($builder, $options);
 
-//        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
-//            $form       = $event->getForm();
-//            $object     = $form->getParent()->get('mappedObject');
-//
-//            if (isset($object)) {
-//                $objectName = $object->getData();
-//
-//                if ('contact' !== $objectName && 'contact' !== $objectName) {
-//                    $form->add(
-//                        'saveRemove',
-//                        ChoiceType::class,
-//                        [
-//                            'attr' => [
-//                                'data-show-on' => '{"formfield_mappedField:data-list-type": "1"}',
-//                                'tooltip'      => 'custom.item.select.mapped_field.action.descr',
-//                            ],
-//                            'label'    => 'custom.item.select.mapped_field.action',
-//                            'choices'  => [
-//                                'Add'    => 1,
-//                                'Remove' => 2,
-//                            ],
-//                        ]
-//                    );
-//                }
-//            }
-//        });
-
         $objects = $this->customObjectModel->getEntities()->getIterator();
-
         foreach ($objects as $object) {
             $objectsArr[] = '"'.$object->getAlias().'"';
         }
@@ -79,9 +47,7 @@ class FormFieldSelectTypeExtension extends AbstractTypeExtension
             [
                 'attr' => [
                     'data-show-on'            => '{"formfield_mappedObject": ['.$objectStr.']}',
-//                    'data-show-on'            => '{"formfield_mappedObject": ["products", "materials"]}',
 //                    'data-show-on'            => '{"formfield_mappedField:data-list-type": "1", "select[name=\"formfield[mappedObject]\"]": ["product", "material"]}',
-//                    select[name="formfield[mappedObject]"]
                     'data-custom-object-prop' => 'true',
                     'tooltip'                 => 'custom.item.select.mapped_field.action.descr',
                 ],

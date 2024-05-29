9f20945c8474729bacab8ea941c8cce6e9736431
rahuld-dev <rahul.dhande@acquia.com>
2024-05-29 18:32:44 +0530

    Adding inTheLast and inTheNext operator for dat and datetime type fields

--- CustomFieldType/DateType.php
+++ CustomFieldType/DateType.php
@@ -76,7 +76,7 @@ class DateType extends AbstractCustomFieldType
     public function getOperators(): array
     {
         $allOperators     = parent::getOperators();
-        $allowedOperators = array_flip(['=', '!=', 'gt', 'gte', 'lt', 'lte', 'empty', '!empty']);
+        $allowedOperators = array_flip(['=', '!=', 'gt', 'gte', 'lt', 'lte', 'empty', '!empty', 'inLast', 'inNext']);
 
         return array_intersect_key($allOperators, $allowedOperators);
     }
